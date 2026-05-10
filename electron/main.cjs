dbbf100633b71cb8729185586b575969e37d61fe
layla <173393835+codedbycupidity@users.noreply.github.com>
2026-05-09 20:13:32 -0400

    Scaffold Electron + Vite + React app

---
+++ electron/main.cjs
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 720,
+    minWidth: 720,
+    minHeight: 480,
+    backgroundColor: '#14101a',
+    titleBarStyle: 'hiddenInset',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
