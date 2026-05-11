ff6694c5e0abf9fd2a3e21c969077eba37f91794
lilly <173393835+codedbycupidity@users.noreply.github.com>
2026-05-11 15:35:16 -0400

    Add audio directory to gitignore, configure vite publicDir, add music-metadata deps

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  publicDir: 'audio',
   server: {
     port: 5173,
     strictPort: true,
